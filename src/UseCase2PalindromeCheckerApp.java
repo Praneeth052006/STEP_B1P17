2d4c34dad3ff0e16545a69e856e4dc33aa363319
Praneeth052006 <pravish.joy@gmail.com>
2026-02-27 10:33:17 +0530

    UC2:Hardcoded palindrome

---
+++ src/UseCase2PalindromeCheckerApp.java
@@ -0,0 +1,23 @@
+
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+
+        String input = "madam";
+
+
+        String reversed = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+
+
+        boolean isPalindrome = input.equals(reversed);
+
+
+        System.out.println("Input: " + input);
+        System.out.println("Is it a palindrome?: " + isPalindrome);
+    }
+}
